0c9c116d28f21563151af8a02736094ea4da7b61
Juno <santosjuno@gmail.com>
2016-03-30 16:58:58 +0400

    backup

---
+++ sandBox/ajaxTest/js/script.js
@@ -0,0 +1,12 @@
+function ajax_post() {
+
+  //creating new XMLHttpRequest object
+  var hr = new XMLHttpRequest();
+
+  var url = "http://localhost/ajaxTest.php";
+  var fn = document.getElementById("firstName").value;
+  var ln = document.getElementById("lastName").value;
+  var c = document.getElementById("comments").value;
+  
+
+}
